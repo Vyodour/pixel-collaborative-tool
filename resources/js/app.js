a567f0b3ea93246d9c687a7d0ca0dabf5c98919c
Roja Fadilah <rojafadilah0203@gmail.com>
2026-01-14 09:14:42 +0800

    fix: import vue file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,15 @@
+import './echo';
 import { createApp } from 'vue';
 import LoginPage from './Pages/Auth/Login.vue';
 import RegisterPage from './Pages/Auth/Register.vue';
 import PixelCanvas from './Components/PixelCanvas.vue';
+import PixelEditor from './Components/PixelEditor.vue';
+import ProjectChat from './Components/ProjectChat.vue';
 
 createApp({})
     .component('login-page', LoginPage)
     .component('register-page', RegisterPage)
     .component('pixel-canvas', PixelCanvas)
+    .component('pixel-editor', PixelEditor)
+    .component('project-chat', ProjectChat)
     .mount('#app');
